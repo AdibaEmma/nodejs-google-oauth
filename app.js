063979c4bd1f06c46ac412d4d54bb74a1d67bb26
Emmanuel Adiba <eabaagah@gmail.com>
2021-02-12 11:21:51 +0000

    view templates and logging

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 require("dotenv").config()
 const express = require("express")
+const exphbs = require("express-handlebars")
+const morgan = require("morgan")
 
 const connectDB = require("./db")
 
 // connect to database
 connectDB()
 
+// express server
 const app = express()
+
+// Logging
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"))
+}
+
+// Handlebars
+app.engine(".hbs", exphbs({defaultLayout: "main", extname: ".hbs"}))
+app.set("view engine", ".hbs")
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> {
