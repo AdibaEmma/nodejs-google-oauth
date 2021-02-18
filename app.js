1864805ef51b119e80828aa2034eb6f2ebefdf64
Emmanuel Adiba <eabaagah@gmail.com>
2021-02-18 12:43:50 +0000

    authenticate user

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ require("dotenv").config()
 const path = require("path")
 const express = require("express")
 const exphbs = require("express-handlebars")
+const passport = require("passport")
+const googleAuth = require("./config/passport")
+const session = require("express-session")
 const morgan = require("morgan")
-
-const connectDB = require("./db")
+const connectDB = require("./config/db")
 const storiesRoutes = require("./routes/index")
+const authRoute = require("./routes/auth")
+
+// Passport config
+googleAuth(passport)
 
 // connect to database
 connectDB()
@@ -22,11 +28,26 @@ if(process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs({defaultLayout: "main", extname: ".hbs"}))
 app.set("view engine", ".hbs")
 
+// Sessions
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session()) 
+
 // Static folder
 app.use(express.static(path.join(__dirname, "public")))
 
 // Routes
 app.use("/", storiesRoutes)
+app.use("/auth", authRoute)
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
