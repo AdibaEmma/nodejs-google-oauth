bb6b91de87c380cf54b03c1e56b3984977bc9b86
Emmanuel Adiba <eabaagah@gmail.com>
2021-02-18 12:45:46 +0000

    update

--- config/passport.js
+++ config/passport.js
@@ -34,5 +34,5 @@ module.exports = (passport) => {
 
     passport.serializeUser((user, cb) => cb(null, user.id))
 
-    passport.deserializeUser( ( id, cb ) => User.findById( id, ( err, user ) => done( err, user ) ) )
+    passport.deserializeUser( ( id, cb ) => User.findById( id, ( err, user ) => cb( err, user ) ) )
 }
