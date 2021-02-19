705b36391fd1be1f2df43dd7f974d3bb365e1861
Emmanuel Adiba <eabaagah@gmail.com>
2021-02-19 12:50:19 +0000

    dashboard

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,23 @@ const express = require("express")
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require("../middleware/auth")
 
+const Story = require("../models/Story")
+
 router.get("/", ensureGuest, (req, res) => {
     res.render("login", {
         layout: "login"
     })
 })
 
-router.get("/dashboard", ensureAuth, (req, res) => {
+// @desc Dashboard
+// @route GET /dashboard
+router.get("/dashboard", ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+    } catch (err) {
+        console.log(err);
+        res.render("error/500")
+    }
     res.render("dashboard", {
         name: req.user.firstName
     })
