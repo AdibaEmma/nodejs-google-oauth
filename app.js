3c8a9227abe480c2086aaacf65e291bad6a95eed
Emmanuel Adiba <eabaagah@gmail.com>
2021-03-03 10:59:08 +0000

    update edit icon

--- app.js
+++ app.js
@@ -36,13 +36,14 @@ if(process.env.NODE_ENV === "development") {
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require("./helpers/hbs")
+const { formatDate, stripTags, truncate, editIcon } = require("./helpers/hbs")
 
 // Handlebars
 app.engine(".hbs", exphbs({helpers: {
     formatDate,
     stripTags, 
-    truncate
+    truncate,
+    editIcon
 },
 defaultLayout: "main", 
 extname: ".hbs"}))
@@ -62,6 +63,11 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session()) 
 
+// Set Global Variables
+app.use( function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 // Static folder
 app.use(express.static(path.join(__dirname, "public")))
 
