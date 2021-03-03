3f3e022a5123172c21145cae009ee56977f14eda
Emmanuel Adiba <eabaagah@gmail.com>
2021-03-03 11:32:03 +0000

    method override

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const exphbs = require("express-handlebars")
+const methodOverride = require("method-override")
 const passport = require("passport")
 const googleAuth = require("./config/passport")
 const session = require("express-session")
@@ -29,6 +30,15 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method Override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
 
 // Logging
 if(process.env.NODE_ENV === "development") {
